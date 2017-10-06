baa9a8a3cbe68e2b9910cd6c142370456d79ced5
Emilie Nagahama <emilienagahama@Emilies-MacBook-Pro.local>
2017-10-06 12:29:25 -0400

    update home.php to page-blog.php

---
+++ wp-content/themes/chloescountertop-v2/template-parts/pages/blog/banner.php
@@ -0,0 +1,4 @@
+<div class="tag-line">
+  <h2 class="h2-tag blue"><span><?php _e('Blog', 'creative_ritual'); ?></span></h2>
+  <h3 class="sub-text"><?php _e('Conscious Living & Intuitive Eating', 'creative_ritual'); ?></h3>
+</div>
